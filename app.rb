d8133b6abb96a1b590840eab88c8b19fab545ca0
vicfriedman <victoria.m.friedman@gmail.com>
2014-12-12 15:51:12 -0500

    views

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'bundler' #require bundler
 Bundler.require
 require 'httparty'
+require_relative 'lib/outdoor.rb'
 
 class MyApp < Sinatra::Base
 
@@ -9,6 +10,10 @@ class MyApp < Sinatra::Base
   end
 
   post '/results' do
+    @restuarants = Neighborhood.new(params[:neighborhood] + ", New York, NY")
+    @restuarants.search
+    @restuarants.venue_ids
+    @results = @restuarants.check_outdoor
     erb :results
   end
 
